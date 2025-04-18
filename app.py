ae65cb9dcbd5faec916d5d7c0af5af97c9f9f3ee
rubenledesma10 <rubenledesma89@gmail.com>
2025-04-18 17:12:41 -0300

    create conexion bd and structure proyect

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask # type: ignore
+from flask import Flask
+from config.config import DATABASE_CONNECTION_URI
+from models.db import db
 
 app = Flask(__name__)
 
+app.config["SQLALCHEMY_DATABASE_URI"]= DATABASE_CONNECTION_URI
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db.init_app(app)
+
+with app.app_context():
+    from models.mechanic import Mechanic
+    # db.drop_all()
+    db.create_all()
+
 if __name__ == '__main__':
     app.run(debug=True)
-
 
 
 
